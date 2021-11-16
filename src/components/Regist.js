c3beb9a19b1e1ff352f3679345d72620874fdd39
Fabian <fabianmendoza.py@gmail.com>
2021-11-16 00:42:40 -0800

    estilo a form

--- src/components/Regist.js
+++ src/components/Regist.js
@@ -1,22 +1,22 @@
 import React from 'react'
 const Regist = () => (
   <div className="Regist">
-    <h3>Registrate</h3>
+    <h1>Registrate</h1>
     <div className="Data">
       
-      <input placeholder="Nombre de usuario"></input>
+      <input className="Inputs" placeholder="Nombre de usuario"></input>
     </div>
     <div className="Data">
       
-      <input placeholder="Nombre"></input>
+      <input className="Inputs" placeholder="Nombre"></input>
     </div>
     <div className="Data">
       
-      <input placeholder="Apellidos"></input>
+      <input className="Inputs" placeholder="Apellidos"></input>
     </div>
     <div className="Data">
       
-      <select id="hh">
+      <select className="Inputs" id="hh">
         <option value="Selecciona">Tipo de usuario</option>
         <option value="Alumno">Alumno</option>
         <option value="Profesor">Profesor</option>
