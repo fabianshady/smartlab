7034e88d5c8785306a6529b9aec1bf15c504fd72
Fabian <fabianhiphop.r.a.p.rf@gmail.com>
2021-11-15 17:28:29 -0800

    formulario terminado

--- src/components/Proys.js
+++ src/components/Proys.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import icon from '../proyector.png';
 
-const Proys = ({ name }) => (
-  <section className="Users">
-    <h3>Aqui se veran los Proyectores</h3>
-    <p>Todos {name}</p>
-  </section>
+const Proys = () => (
+  <div className="Proys">
+    <div>
+      <h1>Proyectores</h1>
+    </div>
+    <div className="Proyectores">
+      <div className="Proy">
+        <img className="Img" src={icon} width="200px" height="200px"></img>
+        <h1>Proyector -1-</h1>
+        <div>
+          <h4 className="Datos">Disponible</h4>
+        </div>
+      </div>
+      <div className="Proy">
+        <img className="Img" src={icon} width="200px" height="200px"></img>
+        <h1>Proyector -2-</h1>
+        <div>
+          <h4 className="DatosN">No disponible</h4>
+        </div>
+      </div>
+      <div className="Proy">
+        <img className="Img" src={icon} width="200px" height="200px"></img>
+        <h1>Proyector -3-</h1>
+        <div>
+        <h4 className="Datos">Disponible</h4>
+        </div>
+      </div>
+      <div className="Proy">
+        <img className="Img" src={icon} width="200px" height="200px"></img>
+        <h1>Proyector -4-</h1>
+        <div>
+          <h4 className="DatosN">No disponible</h4>
+        </div>
+      </div>
+    </div>
+    
+  </div>
 )
 
 export default Proys
--- src/components/Regist.js
+++ src/components/Regist.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
 const Regist = () => (
-  <section className="About">
-    <h3>Aqui se vera el formulario de registro</h3>
-  </section>
+  <div className="Regist">
+    <h3>Registrate</h3>
+    <div className="Data">
+      
+      <input placeholder="Nombre de usuario"></input>
+    </div>
+    <div className="Data">
+      
+      <input placeholder="Nombre"></input>
+    </div>
+    <div className="Data">
+      
+      <input placeholder="Apellidos"></input>
+    </div>
+    <div className="Data">
+      
+      <select id="hh">
+        <option value="Selecciona">Tipo de usuario</option>
+        <option value="Alumno">Alumno</option>
+        <option value="Profesor">Profesor</option>
+      </select>
+    </div>
+    
+    <div className="Data">
+    <button className="But">Enviar</button>
+    </div>
+  </div>
+  
 )
-
 export default Regist
